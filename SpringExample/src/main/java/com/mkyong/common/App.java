a35291bc0c1a553e3e57ea401324d386ba9eee61
Pradeep P Joshi <pradeep.p.joshi@sap.com>
2020-05-13 17:14:05 +0530

    spring bean scope

---
+++ SpringExample/src/main/java/com/mkyong/common/App.java
@@ -0,0 +1,24 @@
+package com.mkyong.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mkyong.customer.services.CustomerService;
+//https://mkyong.com/spring/spring-bean-scopes-examples/
+//Prototype and singleton
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = 
+    		new ClassPathXmlApplicationContext(new String[] {"Spring-Customer.xml"});
+
+    	CustomerService custA = (CustomerService)context.getBean("customerService");
+    	custA.setMessage("Message by custA");
+    	System.out.println("Message : " + custA.getMessage());
+    	
+    	//retrieve it again
+    	CustomerService custB = (CustomerService)context.getBean("customerService");
+    	System.out.println("Message : " + custB.getMessage());
+    }
+}
